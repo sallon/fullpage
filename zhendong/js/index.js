e4dff1be53784cff4e223a64f7fba80aea9871e4
sallon <531601015@qq.com>
2015-03-07 18:32:26 +0800

    sss

--- zhendong/js/index.js
+++ zhendong/js/index.js
@@ -142,6 +142,10 @@ function showScore(){
     $("#score").html(core);
     $("#page-score").removeClass("hide").addClass("animated slideInDown");
     $("#advimg").attr("src","images/9.png")
+    $("title").text("#"+$("#inputIn").val()+"#的三(chun)八(meng)指数竟然高达"+core+"%");
+    setTimeout(function(){
+        ("#inputIn").val("");
+    },1000);
 
 }
 function changePage(num,num1){
@@ -155,17 +159,17 @@ function changePage(num,num1){
 
 }
 function changeFooterImg(num){
-    $("#advimg").addClass("animated flipOutY");
+    $("#advimg").addClass("animated tada");
     setTimeout(function(){
-        $("#advimg").removeClass("animated flipOutY");
+        $("#advimg").removeClass("animated tada");
     },500);
     $("#advimg").attr("src","images/"+num+".png?v=1");
-    setTimeout(function(){
-        $("#advimg").removeClass("hide").addClass("animated flipInX");
-        setTimeout(function(){
-            $("#advimg").removeClass("animated flipInX");
-        },1000);
-    },1000);
+    //setTimeout(function(){
+    //    $("#advimg").removeClass("hide").addClass("animated wobble");
+    //    setTimeout(function(){
+    //        $("#advimg").removeClass("animated wobble");
+    //    },1000);
+    //},500);
 }
 $("#starBtn").on("tap",function(){
     if($("#inputIn").val()=="" || $("#inputIn").val() == "输入TA的姓名"){
@@ -179,58 +183,3 @@ $("#starBtn").on("tap",function(){
         $(".footer").removeClass("hide").addClass("animated rotateInDownLeft");
     }
 })
-
-var imgUrl = 'http://zhendongtg.duapp.com/images/home/share.jpg';  // 分享后展示的一张图片
-var lineLink = 'http://zhendongtg.duapp.com/index.html'; // 点击分享后跳转的页面地址
-var descContent = $("#inputIn").val() + ""+core;  // 分享后的描述信息
-var shareTitle = 'xx';  // 分享后的标题
-var appid = '';  // 应用id,如果有可以填，没有就留空
-
-function shareFriend() {
-    WeixinJSBridge.invoke('sendAppMessage',{
-        "appid": appid,
-        "img_url": imgUrl,
-        "img_width": "200",
-        "img_height": "200",
-        "link": lineLink,
-        "desc": descContent,
-        "title": shareTitle
-    }, function(res) {
-        //_report('send_msg', res.err_msg);  // 这是回调函数，必须注释掉
-    })
-}
-function shareTimeline() {
-    WeixinJSBridge.invoke('shareTimeline',{
-        "img_url": imgUrl,
-        "img_width": "200",
-        "img_height": "200",
-        "link": lineLink,
-        "desc": descContent,
-        "title": shareTitle
-    }, function(res) {
-        //_report('timeline', res.err_msg); // 这是回调函数，必须注释掉
-    });
-}
-function shareWeibo() {
-    WeixinJSBridge.invoke('shareWeibo',{
-        "content": descContent,
-        "url": lineLink
-    }, function(res) {
-        //_report('weibo', res.err_msg);
-    });
-}
-// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
-document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-    // 发送给好友
-    WeixinJSBridge.on('menu:share:appmessage', function(argv){
-        shareFriend();
-    });
-    // 分享到朋友圈
-    WeixinJSBridge.on('menu:share:timeline', function(argv){
-        shareTimeline();
-    });
-    // 分享到微博
-    WeixinJSBridge.on('menu:share:weibo', function(argv){
-        shareWeibo();
-    });
-}, false);
