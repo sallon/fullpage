e570d214b49d61e7f3ef6a40f6b03945622908bb
sallon <531601015@qq.com>
2015-03-17 12:49:48 +0800

    sallon

---
+++ fuzhenghuayu/js/index.js
@@ -0,0 +1,91 @@
+/**
+ * Created by sallon on 2015/3/17.
+ */
+$(function(){
+    $("#page1").swipeUp(function(){
+        showPgaeTwo();
+    });
+    $("#page2").swipeUp(function(){
+        showPageThree();
+    });
+    $("#page3").swipeUp(function(){
+        showPageFour();
+    });
+    $("#page4").swipeUp(function(){
+        showPageFive();
+    });
+    $("#page5").swipeUp(function(){
+        showPageSix();
+    });
+});
+/*显示第一页*/
+function showPageOne(){
+    $("#page1").css("display","block").addClass("animated fadeIn");
+    $("#text1").css("display","block").addClass("animated fadeInLeft");
+    setTimeout(function(){
+        $("#page1").removeClass("animated fadeIn");
+        $("#text1").removeClass("animated fadeInLeft");
+    },1000);
+}
+/*显示第二页*/
+function showPgaeTwo(){
+    $("#page1").addClass("animated fadeOutUp");
+    $("#page2").css("display","block").addClass("animated fadeInUp");
+    setTimeout(function(){
+        $("#text2").css("display","block").addClass("animated fadeInDown");
+    },1000)
+    setTimeout(function(){
+        $(".map").css("display","block").addClass("animated zoomIn");
+    },2000)
+    setTimeout(function(){
+        $(".yuan1").css("display","block").addClass("animated zoomIn");
+    },2500)
+    setTimeout(function(){
+        $(".yuan2").css("display","block").addClass("animated zoomIn");
+    },3000)
+    setTimeout(function(){
+        $(".yuan3").css("display","block").addClass("animated zoomIn");
+    },3500)
+}
+/*显示第三页*/
+function showPageThree(){
+    $("#page2").addClass("animated fadeOutUp");
+    $("#page3").css("display","block").addClass("animated fadeInUp");
+    setTimeout(function(){
+        $(".gan").css("display","block").addClass("animated fadeInDown");
+    },1000)
+    setTimeout(function(){
+        $("#text3").css("display","block").addClass("animated fadeInUp");
+    },2000)
+}
+/*显示第四页*/
+function showPageFour(){
+    $("#page3").addClass("animated fadeOutUp");
+    $("#page4").css("display","block").addClass("animated fadeInUp");
+    setTimeout(function(){
+        $("#text4").css("display","block").addClass("animated fadeInDown");
+    },1000)
+}
+
+/*显示第五页*/
+function showPageFive(){
+    $("#page4").addClass("animated fadeOutUp");
+    $("#page5").css("display","block").addClass("animated fadeInUp");
+    setTimeout(function(){
+        $("#text5").css("display","block").addClass("animated fadeInDown");
+    },1000)
+}
+/*显示第六页*/
+function showPageSix(){
+    $("#page5").addClass("animated fadeOutUp");
+    $("#page6").css("display","block").addClass("animated zoomIn");
+    setTimeout(function(){
+        $(".xin").css("display","block").addClass("animated zoomIn");
+    },1000)
+    setTimeout(function(){
+        $("#text6").css("display","block").addClass("animated fadeInDown");
+    },2000)
+    setTimeout(function(){
+        $(".bottomImg").css("display","block").addClass("animated fadeInUp");
+    },2500)
+}
