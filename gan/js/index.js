8ec397d543a511e1cbef6f4abbdec71bb5d8ee58
sallon <531601015@qq.com>
2015-03-17 22:44:23 +0800

    sallon

--- gan/js/index.js
+++ gan/js/index.js
@@ -14,9 +14,17 @@ $(function(){
     $("#page3").swipeUp(function(){
         showPageFour();
     });
+    $("#page3").swipeDown(function(){//第三页向上滑上去显示第二页
+        showPgaeTwoAgain();
+    });
+
     $("#page4").swipeUp(function(){
         showPageFive();
     });
+    $("#page4").swipeUp(function(){//第四页向上滑上去显示第三页
+        showPageThreeAgain();
+    });
+
     $("#page5").swipeUp(function(){
         showPageSix();
     });
@@ -28,15 +36,19 @@ $(function(){
 function showPageOne(){
     $("#page1").css("display","block").addClass("animated fadeIn");
     $("#text1").css("display","block").addClass("animated fadeInLeft");
-    //setTimeout(function(){
-    //    $("#page1").css("display","none").removeClass("animated fadeIn");
-    //    $("#text1").css("display","none").removeClass("animated fadeInLeft");
-    //},1000);
 }
 function showPageOneAgain(){
     $("#page2").addClass("animated fadeOutDown");
     $("#page1").css("display","block").removeClass("animated fadeOutUp").addClass("animated fadeIn");
     $("#text1").css("display","block").addClass("animated fadeInLeft");
+    //清楚第二页上面的动画
+    $("#page2").css("display","none").removeClass("animated fadeInUp fadeOutDown");
+    $("#text2").css("display","none").removeClass("animated fadeInDown");
+    $(".map").css("display","none").removeClass("animated zoomIn");
+    $(".yuan1").css("display","none").removeClass("animated zoomIn");
+    $(".yuan2").css("display","none").removeClass("animated zoomIn");
+    $(".yuan3").css("display","none").removeClass("animated zoomIn");
+    $(".yuan3").css("display","none").removeClass("animated zoomIn");
 }
 /*显示第二页*/
 function showPgaeTwo(){
@@ -60,10 +72,38 @@ function showPgaeTwo(){
     setTimeout(function(){
         $(".yuan3").css("display","block").addClass("animated zoomIn");
     },3500)
-    setTimeout(function(){
-
-    },3500)
+    //清除第一页上面的动画class
+    $("#page1").css("display","none").removeClass("animated fadeIn fadeOutUp");
+    $("#text1").css("display","none").removeClass("animated fadeInLeft");
 }
+
+function showPgaeTwoAgain(){
+    $("#page3").addClass("animated fadeOutDown");
+    $("#page2").css("display","block").addClass("animated fadeInDown");
+    setTimeout(function(){
+        $("#text2").css("display","block").addClass("animated fadeInDown");
+    },1000)
+    setTimeout(function(){
+        $(".map").css("display","block").addClass("animated zoomIn");
+    },2000)
+    setTimeout(function(){
+        $(".yuan1").css("display","block").addClass("animated zoomIn");
+    },2500)
+    setTimeout(function(){
+        $(".yuan2").css("display","block").addClass("animated zoomIn");
+    },3000)
+    setTimeout(function(){
+        $(".yuan3").css("display","block").addClass("animated zoomIn");
+    },3500)
+    setTimeout(function(){
+        $(".yuan3").css("display","block").addClass("animated zoomIn");
+    },3500)
+    //清楚第三页上面的动画
+    $("#page3").css("display","none").removeClass("animated fadeInUp fadeOutDown");
+    $(".gan").css("display","none").removeClass("animated fadeInDown");
+    $("#text3").css("display","none").removeClass("animated fadeInUp");
+}
+
 /*显示第三页*/
 function showPageThree(){
     $("#page2").addClass("animated fadeOutUp");
@@ -74,7 +114,30 @@ function showPageThree(){
     setTimeout(function(){
         $("#text3").css("display","block").addClass("animated fadeInUp");
     },2000)
+    //清楚第二页上面的动画
+    $("#page2").css("display","none").removeClass("animated fadeInUp fadeOutUp");
+    $("#text2").css("display","none").removeClass("animated fadeInDown");
+    $(".map").css("display","none").removeClass("animated zoomIn");
+    $(".yuan1").css("display","none").removeClass("animated zoomIn");
+    $(".yuan2").css("display","none").removeClass("animated zoomIn");
+    $(".yuan3").css("display","none").removeClass("animated zoomIn");
+    $(".yuan3").css("display","none").removeClass("animated zoomIn");
 }
+
+function showPageThreeAgain(){
+    $("#page4").addClass("animated fadeOutDown");
+    $("#page3").css("display","block").addClass("animated fadeInUp");
+    setTimeout(function(){
+        $(".gan").css("display","block").addClass("animated fadeInDown");
+    },1000)
+    setTimeout(function(){
+        $("#text3").css("display","block").addClass("animated fadeInUp");
+    },2000)
+    //清楚第四页上面的动画
+    $("#page4").css("display","none").removeClass("animated fadeInUp fadeOutDown");
+    $("#text4").css("display","none").removeClass("animated fadeInDown");
+}
+
 /*显示第四页*/
 function showPageFour(){
     $("#page3").addClass("animated fadeOutUp");
@@ -82,6 +145,10 @@ function showPageFour(){
     setTimeout(function(){
         $("#text4").css("display","block").addClass("animated fadeInDown");
     },1000)
+    //清楚第三页上面的动画
+    $("#page3").css("display","none").removeClass("animated fadeInUp fadeOutUp");
+    $(".gan").css("display","none").removeClass("animated fadeInDown");
+    $("#text3").css("display","none").removeClass("animated fadeInUp");
 }
 
 /*显示第五页*/
@@ -91,6 +158,9 @@ function showPageFive(){
     setTimeout(function(){
         $("#text5").css("display","block").addClass("animated fadeInDown");
     },1000)
+    //清楚第四页上面的动画
+    $("#page4").css("display","none").removeClass("animated fadeInUp fadeOutUp");
+    $("#text4").css("display","none").removeClass("animated fadeInDown");
 }
 /*显示第六页*/
 function showPageSix(){
@@ -105,6 +175,9 @@ function showPageSix(){
     setTimeout(function(){
         $(".bottomImg").css("display","block").addClass("animated fadeInUp");
     },2500)
+    //清楚第五页上面的动画
+    $("#page5").css("display","none").removeClass("animated fadeInUp fadeOutUp");
+    $("#text5").css("display","none").removeClass("animated fadeInDown");
 }
 /*显示第七页*/
 function showPageSeven(){
@@ -112,13 +185,4 @@ function showPageSeven(){
     $(".swipeImg").css("display","none");
     $("#page7").css("display","block").addClass("animated fadeInUp");
 
-    //setTimeout(function(){
-    //    $(".xin").css("display","block").addClass("animated zoomIn");
-    //},1000)
-    //setTimeout(function(){
-    //    $("#text6").css("display","block").addClass("animated fadeInDown");
-    //},2000)
-    //setTimeout(function(){
-    //    $(".bottomImg").css("display","block").addClass("animated fadeInUp");
-    //},2500)
 }
