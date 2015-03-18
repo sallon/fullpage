8a8ef9179eecf454cd9da70a55596dcd6123e56f
sallon <531601015@qq.com>
2015-03-18 23:09:53 +0800

    sallon

---
+++ gan/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Created on 2015-3-17
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+  require("index.html");
+?>
